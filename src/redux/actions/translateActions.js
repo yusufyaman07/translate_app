2e2d5338749715500fce34fce3d3cdf17fb06148
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-03-04 10:53:24 +0300

    first commit

---
+++ src/redux/actions/translateActions.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { languageOptions } from "../../constants";
+
+export const getLanguages = createAsyncThunk(
+  "language/getLanguages",
+  async () => {
+    const res = await axios.request(languageOptions);
+
+    return res.data.data.languages;
+  }
+);
+
+export const translateText = createAsyncThunk(
+  "translate/translateText",
+  async action_params => {
+    const { sourceLang, targetLang, text } = action_params;
+
+    const params = new URLSearchParams();
+    params.set("source_language", sourceLang.value);
+    params.set("target_language", targetLang.value);
+    params.set("text", text);
+
+    const options = {
+      method: "POST",
+      url: "https://text-translator2.p.rapidapi.com/translate",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "75dc092df0msh3c03138e5cc1ea2p19035ejsn916bcc592247",
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+      },
+      data: params,
+    };
+
+    const res = await axios.request(options);
+    console.log(res.data);
+
+    return res.data.data.translatedText;
+  }
+);
---
+++ src/redux/slices/translateSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { translateText } from "../actions/translateActions";
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  text: "",
+};
+
+const translateSlice = createSlice({
+  name: "translate",
+  initialState,
+
+  reducers: {
+    updateText: (state, action) => {
+      state.text = action.payload;
+    },
+  },
+
+  extraReducers: builder => {
+    builder.addCase(translateText.pending, state => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(translateText.rejected, (state, action) => {
+      alert(
+        "İşlem gerçekleşirken bir sorun oluştu lütfen daha sonra tekrar deneyin"
+      );
+      state.isLoading = false;
+      state.isError = action.error;
+    });
+
+    builder.addCase(translateText.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.text = action.payload;
+    });
+  },
+});
+
+export default translateSlice.reducer;
+export const { updateText } = translateSlice.actions;
